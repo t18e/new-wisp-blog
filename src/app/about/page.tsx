73cd314ff4ec41cfdb42ce1420a5ff52b57899f7
t18e <tiwaadejuyigbe@gmail.com>
2025-02-06 18:02:39 +0000

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,28 +7,33 @@ import Markdown from "react-markdown";
 const content = `
 
 ## Some things about me:
-* Politics at UCL with a focus on innovation policy, political theory and international political economy (IPE). 
-* Currently interested in the implications of digital monism on relationships, consumption psychologies and political ideologies.
-* Working on reading way more fiction with the help of [the best recommendation engine](https://en.app/).
+* Studying Politics at UCL with a focus on innovation policy, political theory and international political economy (IPE). 
+* Reading way more fiction with the help of [the best recommendation engine](https://en.app/) out there.
+* Writing about the implications of digital monism on relationships, consumption psychologies and political ideologies.
 
 ## Some things I've worked on:
 I enjoy ideating & building for underserved markets, and I've been lucky to work on a few interesting ventures](https://t18e.com/blog/idea-ball) during my time at UCL. Here are a few of my favourites:
 
 * Brade: a plug-and-play accounting tool for salons and other beauty services. We built Brade to make reconciling multi-channel payments, forecasting finances and filing taxes 10x cheaper and 100x easier.
-* Pubbler: a writing assistant specifically for Substack writers built on Claude Sonnet to provide tailored writing feedback. We , but it didn't feel right. 
+* Pubbler: a self-learning writing assistant specifically for Substack writers built on Anthropic's Claude Sonnet model. Despite positive waitlist signalling, it didn't feel right, and I decided not to launch. 
 * REN: a wearable ring for individuals struggling with addictions to discreetly seek help via touch sensor & BLE. REN was designed based on Prochaska and DiClemente’s transtheoretical model of behavioral change.
-* Soundtrack: a 'song a day' social media platform based on the tried-and-tested streak model by BeReal and Snapchat. I advised on Soundtrack's product and on-campus marketing strategies.
+* Soundtrack: a 'song a day' social media platform based on the tried-and-tested streak model by BeReal and Snapchat. I advised on Soundtrack's design, product and on-campus marketing strategies.
 * Banga: a WhatsApp booking agent for Africa's restaurants. During testing, Banga automated the entire process via WhatsApp, from booking to email confirmations, cutting booking time by 80%.
 * Almanac: an AMA, Reddit-style forum for student-alumni networking and mentorship. I built this specifically for my alma mater, but it unfortunately didn't materialise. 
 
 It's uncertain why some of these didn't succeed as expected. In some cases, it was simply a matter of [inertia](https://t18e.com/blog/inertia). In other instances, it's quite possible we 'overniched' and were just 1-2 pivots away from getting it right. 
 
-## Cue:
-* Politics at UCL with a focus on innovation policy, political theory and international political economy (IPE). 
-* Currently interested in the implications of digital monism on relationships, consumption psychologies and political ideologies.
-* Working on reading way more fiction with the help of [the best recommendation engine](https://en.app/).
+## Some things I'm interested in:
+* Digital monism
+* Sociolinguistics
+* Metalinguistics
+* Blockchain identity management
+* Universal Basic Income
+* Christian universalism
+* The shadow gig economy
+* The right to die
 
-In the meantime, you can reach me via [email](mailto:mail@t18e.com) or [LinkedIn](https://linkedin.com/in/tiwa-adejuyigbe) with any questions.
+If you've got any questions, you can reach me via [email](mailto:mail@t18e.com) or [LinkedIn](https://linkedin.com/in/tiwa-adejuyigbe).
 
 
 `;
